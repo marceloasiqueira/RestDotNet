2f7b1d7cff2ceb6d7588e9ea07f283212eec8d49
marceloasiqueira <mar.siqueira@gmail.com>
2018-12-17 20:42:41 -0200

    Http Verbs Conclusion

---
+++ 02 - RestDotNet - NovosVerbos/RestDotNet/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestDotNet.Model;
+
+namespace RestDotNet.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        // Contador responsável por gerar um fake ID já 
+        // que não estamos acessando nenhum BD
+        private volatile int count;
+        
+        // Metodo responsavel por criar uma nova pessoa
+        // Se tivéssemos um BD, esse seria o momento de
+        // persistir os dados
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        // Metodo responsavel por retornar uma pessoa
+        // Como não estamos acessando nenhuma base de
+        // dados, estamos retornando um mock
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Mi Nombre",
+                LastName = "Aooooo",
+                Address = "Brasília - DF",
+                Gender = "Male"
+            };
+        }
+
+        // Método responsavel por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            // A nossa lógica de exclusão virá aqui...
+        }
+
+        // Método responsável por retornar todas as
+        // pessoas. Mais uma vez, essas informações
+        // são mock
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i< 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        // Método responsável por atualizar uma pessoa
+        // por ser mock retornamos a mesma informação
+        // passada
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person Lastname " + i,
+                Address = "Some Address " + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
--- 02 - RestDotNet - NovosVerbos/RestDotNet/Startup.cs
+++ 02 - RestDotNet - NovosVerbos/RestDotNet/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestDotNet.Services;
+using RestDotNet.Services.Implementations;
 
 namespace RestDotNet
 {
@@ -25,7 +27,11 @@ namespace RestDotNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Framework Service
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
