8556d4bf919c568fcff31d770c75c129bd8633d8
Marcelo.Siqueira <86017594168@ssp.df.gov.br>
2018-12-18 11:07:27 -0200

    DB Access and REST Crud in practice

---
+++ 03 - RestDotNet - Connect Database/RestDotNet/Model/Context/MySQLContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestDotNet.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
